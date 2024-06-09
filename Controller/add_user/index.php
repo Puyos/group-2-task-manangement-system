be086faf927156964c844f4fa77ae47b59ed7781
Jerniel Serat <146813974+Jerniel@users.noreply.github.com>
2024-06-09 11:52:36 +0800

    Update index.php

--- Controller/add_user/index.php
+++ Controller/add_user/index.php
@@ -1,9 +1,11 @@
 <?php
 
+
 if($_SERVER['REQUEST_METHOD'] == "POST")
 {
     $conn = new Database();
 
+    
     $result = $conn->query("INSERT INTO create_user(fname, lname, image, email, password, role) VALUES(:fname, :lname, :image, :email, :password, :role)", [
         "fname" => $_POST['fname'],
         "lname" => $_POST['lname'],
@@ -13,7 +15,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
         "role" => $_POST['role']
     ]);
 
+    
 if(!$result) redirect("/create_user");
+
     
     redirect("/create_user");
 }
